7ff78749f8d3a4ab670769153eec4de7275adc42
Vlatko Kosturjak <kost@linux.hr>
2019-12-21 20:08:44 +0100

    bump version to v0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iwebrepl",
-    version="0.1.0",
+    version="0.2.0",
     author="Vlatko Kosturjak",
     author_email="vlatko.kosturjak@gmail.com",
     description="Client to handle micropython web_repl interactively",
