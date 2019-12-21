b5512272277f8db67105cb00486208d21bf11a56
Vlatko Kosturjak <kost@linux.hr>
2019-12-21 14:42:24 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="iwebrepl",
+    version="0.1.0",
+    author="Vlatko Kosturjak",
+    author_email="vlatko.kosturjak@gmail.com",
+    description="Client to handle micropython web_repl interactively",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kost/iwebrepl-python",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "websocket-client",
+        "prompt_toolkit",
+        "getpass"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    scripts=[
+        'scripts/iwebrepl'
+    ]
+)
